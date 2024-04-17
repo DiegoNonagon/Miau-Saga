8885fe2ea787595a247c0e9624f0b47800a22102
Diego Cruz <diegoch9630@gmail.com>
2024-04-17 17:14:36 -0600

    Corrección de entrega

--- app.js
+++ app.js
@@ -1,119 +1,144 @@
-//Titulo
-alert ("===MIAU SAGA===")
-
 //Introducción
-alert ("Prólogo")
-alert ("En las lejanas tierras de MichiKron nace un gran poder con la capacidad de darle el reinado a un michi.")
-alert ("El catnip supremo. Sólo uno se alzará ante los demás.")
+
+function intro(){
+    alert ("===MIAU SAGA===");
+    alert ("En las lejanas tierras de MichiKron nace un gran poder con la capacidad de darle el reinado a un michi.");
+    alert ("El catnip supremo. Sólo uno se alzará ante los demás.");
+}
 
 //Selección de personaje
 
 const personajes = ["Michi Ultraespadón", "Michi Katana", "Michi Mago"]
 const personajeRespuestas = ["A", "B", "C"]
-const personajeTextos = ["\n A)  Michi Ultraespadón", "\n B)  Michi Katana", "\n C)  Michi Mago"]
+const textoValidacion = "\n Ingresa A, B o C para escoger."
 
-let loop = true
-while (loop){
-    
-const textoValidacion = "\n Escoge ingresando A, B o C."
+function escogerPersonaje(){
+    const personaje = prompt("Escoge a tu personaje " + "\n A) Michi Ultraespadón" + "\n B) Michi Katana" + "\n C) Michi Mago" + textoValidacion).toUpperCase()
 
-let escogerPersonaje = ""
-
-escogerPersonaje = prompt("Escoge a tu personaje " + personajeTextos + textoValidacion).toUpperCase()
-if (escogerPersonaje == personajeRespuestas[0]){
-    alert ("Escogiste a " + personajes[0])
+    if (personaje == personajeRespuestas[0]){
+        alert ("Escogiste a " + personajes[0])
+    } else if (personaje == personajeRespuestas[1]){
+        alert ("Escogiste a " + personajes[1])
+    } else if (personaje == personajeRespuestas[2]){
+        alert ("Escogiste a " + personajes[2])
+    } else {
+        alert (textoValidacion + "\n Vuelve a escoger.");
+        escogerPersonaje();
+    }
 }
-else if (escogerPersonaje == personajeRespuestas[1]){
-    alert ("Escogiste a " + personajes[1])
-}
-else if (escogerPersonaje == personajeRespuestas[2]){
-    alert ("Escogiste a " + personajes[2])
-}
-else {
-    alert (textoValidacion + "\n Vuelve a escoger")
-}
-
-loop = confirm ("¿Quieres cambiar a tu personaje?" + "\n Preciona cáncelar para continuar con el juego")
-}
-
 //Nombrar personaje
 
 let nombre = ""
-nombre = prompt("Ponle un nombre a tu Michi")
-alert ("Tú Michi se llama " + nombre)
+
+function nombrarPersonaje() {
+
+    nombre = prompt("Ponle un nombre a tu Michi.")
+    alert ("Tú Michi se llama " + nombre)
+
+    if (nombre.trim()===""){
+        alert("Nombre invalido, ingresar de nuevo.");
+        nombrarPersonaje();
+    }
+}
 
 //Nivel 1
-alert ("Nivel 1 Bosques de suculentas")
-alert ("Los Michis del pasado se levantan en busca de poder.")
 
-alert ("Te encuentras con Michi garras salvajes.")
-
+function nivelUno(){
+    alert ("Nivel 1 Bosques de suculentas");
+    alert ("Los Michis del pasado se levantan en busca de poder.");
+    alert ("Te encuentras con Michi garras salvajes.");
+}
 //Combate
 
-let loopCombate = true
-while (loopCombate){
-
-let personaje = prompt ("ingresa 0 para usar Magia, 1 para usar Defensa o 2 Para usar Espada")
-if (personaje == 0) {
-    alert (nombre + " Usó Rayo de luz nocturna")
-}
-else if (personaje == 1) {
-    alert (nombre + " Usó Parry de Escudo")
-}
-else if (personaje == 2) {
-    alert (nombre + " Usó Ataque en salto")
-}
-else {
-    alert ("Solo ingresa 0, 1 o 2" + "\n Vuelve a escoger")
+//Arreglar sistema de puntos
+/*function puntos(a,b){
+    a + b;
+    if(a == 3){
+        return("Ganaste")
+    } else {
+        combate();
+    }
 }
 
-let enemigo = Math.round(Math.random() * 2)
+function puntosEnemigo(a,b){
+    a + b;
+    if(a == 5){
+        return("Has Muerto")
+    } else {
+        combate();
+    }
+}*/
 
-if (enemigo == 0) {
-    alert ("Enemigo uso Lanzar piedra")
-}
-else if (personaje == 1) {
-    alert ("Enemigo uso Esquive con garrazo")
-}
-else if (personaje == 2) {
-    alert ("Enemigo uso agarre de mordida sangrienta")
-}
-
-if (personaje == enemigo) {
-    alert ("Se neutralizaron los ataques")
-} 
-else if (personaje == 0 && enemigo == 2) {
-    alert ("Acertaste el ataque")
-}
-else if (personaje == 1 && enemigo == 0) {
-    alert ("Acertaste el ataque")
-}
-else if (personaje == 2 && enemigo == 1) {
-    alert ("Acertaste el ataque")
-}
-else {
-    alert ("Recibiste el ataque")
+function combate(){
+    atacar();
+    ataqueEnemigo();
+    compararAtaques();
 }
 
-loopCombate = confirm ("¿Quieres atacar de nuevo?")
+let jugador = ""
+
+function atacar(){
+    jugador = prompt ("ingresa 0 para usar Magia, 1 para usar Defensa o 2 Para usar Espada")
+
+    if (jugador == 0) {
+        alert (nombre + " Usó Rayo de luz nocturna");
+        ataqueEnemigo();
+    } else if (jugador == 1) {
+        alert (nombre + " Usó Parry de Escudo");
+        ataqueEnemigo();
+    } else if (jugador == 2) {
+        alert (nombre + " Usó Ataque en salto");
+        ataqueEnemigo();
+    } else {
+        alert ("Solo ingresa 0, 1 o 2" + "\n Vuelve a escoger");
+        atacar();
+    }
 }
 
-let combate = Math.round(Math.random() * 2)
-if (combate == 0) {
-    alert ("Ganaste")
+let enemigo = ""
+
+function ataqueEnemigo(){
+    enemigo = Math.round(Math.random() * 2)
+
+    if (enemigo == 0) {
+        alert ("Enemigo uso Lanzar piedra");
+        compararAtaques();
+    } else if (enemigo == 1) {
+        alert ("Enemigo uso Esquive con garrazo");
+        compararAtaques();
+    } else if (enemigo == 2) {
+        alert ("Enemigo uso agarre de mordida sangrienta");
+        compararAtaques();
+    }
 }
-else if (combate == 1) {
-    alert ("Ganaste")
+
+function compararAtaques(){
+    if (jugador == enemigo) {
+        alert ("Se neutralizaron los ataques");
+        combate();
+    } else if (jugador == 0 && enemigo == 2) {
+        alert ("Acertaste el ataque");
+        puntos(0,1);
+    } else if (jugador == 1 && enemigo == 0) {
+        alert ("Acertaste el ataque");
+        puntos(0,1);
+    } else if (jugador == 2 && enemigo == 1) {
+        alert ("Acertaste el ataque");
+        puntos(0,1);
+    } else {
+        alert ("Recibiste el ataque");
+        puntosEnemigo(0,1);
+    } 
 }
-else {
-    alert ("Perdiste")
+
+//Ejecución
+
+function miauSaga(){
+    intro();
+    escogerPersonaje();
+    nombrarPersonaje();
+    nivelUno();
+    combate();
 }
 
-
-
-
-
-
-
-
-
+miauSaga();
